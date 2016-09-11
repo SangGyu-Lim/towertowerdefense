59e91e28a37dfe3f02e8a40d36a4b108351a651b
limSG <limsg0510@gmail.com>
2016-09-11 18:41:22 +0900

    주석 추가

--- test/Assets/Network.cs
+++ test/Assets/Network.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class Network : MonoBehaviour {
 
+    // 필요에 따라 url을 수정한다.
     string url = "http://localhost/connect.php";
 	// Use this for initialization
 	void Start () {
@@ -17,24 +18,24 @@ public class Network : MonoBehaviour {
 
     public void ConnectServer()
     {
+        // 송신할 데이터 셋팅
         WWWForm sendData = new WWWForm();
+        // addfield에서 비교할 키값, 데이터 값 순서.
         sendData.AddField("characterKey", "1");
+        // 데이터 송신
         WWW www = new WWW(url, sendData);
         StartCoroutine(WaitForRequest(www));
     }
 
 
-    //public WWW GET()
-    //{
-    //    WWW www = new WWW(url);
-    //    StartCoroutine(WaitForRequest(www));
-    //}
-
+    // 테스트 코드
     public void DisConnectServer()
     {
         Debug.Log("call disconnect server");
     }
 
+
+    // coroutine 및 출력
     private IEnumerator WaitForRequest(WWW www)
     {
         yield return www;
