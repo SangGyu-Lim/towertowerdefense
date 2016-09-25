8480f9c14043b8a6bd581f6c4a76fb3444240333
limSG <limsg0510@gmail.com>
2016-09-25 17:37:27 +0900

    초기 화면 ui

---
+++ test/Assets/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+    //GameObject netManager = GameObject.Find("NetworkManager");
+
+    public void btnLogin()
+    {
+        Debug.Log("btn login");
+        //netManager.SendMessage("testLim", 5);
+    }
+
+    public void btnJoin()
+    {
+        Debug.Log("btn join");
+    }
+}
