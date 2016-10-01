c93952a557454eb2d6b24af0449fdbacadff8d05
limSG <limsg0510@gmail.com>
2016-10-01 17:43:10 +0900

    로그인

--- test/Assets/Network.cs
+++ test/Assets/Network.cs
@@ -4,21 +4,40 @@ using UnityEngine.UI;
 
 public class Network : MonoBehaviour {
 
-    // 필요에 따라 url을 수정한다.
-    string url = "http://192.168.0.116/connect.php";
-
-    public void testLim()
+    enum eNetworkState
     {
+        eNONE = 0,
+        eJOIN = 1,
+        eLOGIN = 2
     }
 
-    void ConnectServer()
+    // 필요에 따라 url을 수정한다.
+    string url = "http://localhost/ll.php";
+
+    eNetworkState currentState { get; set; }
+    public string id { get; set; }
+    public string passWord { get; set; }
+    public string eMail { get; set; }
+        
+    void ConnectServer(int state)
     {
         // 송신할 데이터 셋팅
         WWWForm sendData = new WWWForm();
+
+        Debug.LogError(state);
+
+        currentState = (eNetworkState)state;
+        Debug.LogError((int)currentState);
         // addfield에서 비교할 키값, 데이터 값 순서.
-        sendData.AddField("funcName", "testFunction");
-        sendData.AddField("ID", "pjh");
-        sendData.AddField("passWord", "qwe123");
+        sendData.AddField("functionName", (int)currentState );
+        sendData.AddField("ID", id);
+        sendData.AddField("passWord", passWord);
+
+        
+        if(currentState == eNetworkState.eJOIN)
+            sendData.AddField("eMail", eMail);
+
+        Debug.LogError((int)currentState + " / " + id + " / " + passWord + " / " + eMail);
         
         // 데이터 송신
         WWW www = new WWW(url, sendData);
@@ -38,18 +57,23 @@ public class Network : MonoBehaviour {
     {
         yield return www;
 
+
+
         if (www.error == null)
         {
-            Debug.Log("www success\n");
+            Debug.LogError("www success\n");
+            Debug.LogError(www.text);
+            //string[] dataTexts = www.text.Split(',');
 
-            string[] dataTexts = www.text.Split(',');
-
-            foreach (string dataText in dataTexts)
-                Debug.Log(dataText);
+            
+            //foreach (string dataText in dataTexts)
+            //    //Debug.Log(dataText);
+            //    Debug.LogError(dataText);
         }
         else
         {
-            Debug.Log("www error : " + www.error);
+            Debug.LogError("www error : " + www.error);
         }
     }
 }
+
--- test/Assets/UIManager.cs
+++ test/Assets/UIManager.cs
@@ -3,27 +3,97 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
+    enum eState
+    {
+        eNONE = 0,
+        eJOIN = 1,
+        eLOGIN = 2
+    }
+
+    GameObject netManager;
+
     public UIInput inputId;
     public UIInput inputPwd;
+    public UIInput inputEmail;
+
+    eState currentState;
+
+
 
     public void btnLogin()
     {
         Debug.Log("btn login");
 
-        GameObject netManager = GameObject.Find("NetworkManager");
+        currentState = eState.eLOGIN;
 
-        netManager.SendMessage("ConnectServer");
-        
+        connectNetwork();
 
     }
 
     public void btnJoin()
     {
+        changeActiveGameObject("ButtonJoin", false);
+        changeActiveGameObject("ButtonLogin", false);
 
-        GameObject netManager = GameObject.Find("NetworkManager");
+        inputEmail.gameObject.SetActive(true);
+        changeActiveGameObject("ButtonJoinMember", true);
+        changeActiveGameObject("ButtonBack", true);
 
-        netManager.SendMessage("testLim");
+        Debug.Log("btn join");
+    }
 
-        Debug.Log("btn join");       
+    public void btnJoinMember()
+    {
+        currentState = eState.eJOIN;        
+
+        connectNetwork();
+
+        btnBack();
+        Debug.Log("btn join member");
+    }
+
+    public void btnBack()
+    {
+        changeActiveGameObject("ButtonJoin", true);
+        changeActiveGameObject("ButtonLogin", true);
+
+        inputEmail.gameObject.SetActive(false);
+        changeActiveGameObject("ButtonJoinMember", false);
+        changeActiveGameObject("ButtonBack", false);
+
+        Debug.Log("btn back");
+    }
+
+    void connectNetwork()
+    {
+        netManager = GameObject.Find("Network");
+        
+        netManager.gameObject.GetComponent<Network>().id = inputId.value.ToString();
+        netManager.gameObject.GetComponent<Network>().passWord = inputPwd.value.ToString();
+
+        if (currentState == eState.eJOIN)
+            netManager.gameObject.GetComponent<Network>().eMail = inputEmail.value.ToString();
+
+        netManager.SendMessage("ConnectServer", (int)currentState);
+    }
+
+    void changeActiveGameObject(string str, bool state)
+    {
+        Debug.Log(str + state);
+
+		if (state)
+        {
+			//GameObject goTemp = GameObject.Find ("Btn").transform.FindChild (str).gameObject.SetActive (state);
+            GameObject goTemp = GameObject.Find("Btn");
+            goTemp.transform.FindChild(str).gameObject.SetActive(state);
+		}
+		else
+		{
+			//GameObject goTemp = GameObject.Find (str).gameObject.SetActive (state);
+            GameObject goTemp = GameObject.Find(str);
+            goTemp.gameObject.SetActive(state);
+		}
+
+        Debug.Log(str + " completion");
     }
 }
