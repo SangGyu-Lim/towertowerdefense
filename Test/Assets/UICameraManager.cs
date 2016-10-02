a289956f1235dc36b841ba3ac22879ef30c46be4
limSG <limsg0510@gmail.com>
2016-10-03 00:13:16 +0900

    카메라 drag test

--- Test/Assets/UICameraManager.cs
+++ Test/Assets/UICameraManager.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class UICameraManager : MonoBehaviour {
 
+	public float speed = 1.0f;
+	public Vector2 nowPos, prePos;
+	public Vector3 movePos;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +15,25 @@ public class UICameraManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Debug.Log ("camera manager update");
+
+		if (Input.touchCount == 1)
+		{
+			Touch touch = Input.GetTouch (0);
+			if (touch.phase == TouchPhase.Began)
+			{
+				prePos = touch.position - touch.deltaPosition;
+			}
+			else if (touch.phase == TouchPhase.Moved)
+			{
+				nowPos = touch.position - touch.deltaPosition;
+				movePos = (Vector3)(prePos - nowPos) * speed;
+				GetComponent<Camera>().transform.Translate (movePos);
+				prePos = touch.position - touch.deltaPosition;
+			}
+			else if (touch.phase == TouchPhase.Ended)
+			{
+			}
+		}
+
 	}
 }
