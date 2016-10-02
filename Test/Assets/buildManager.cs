1f69b941b5041725ffbf9860eac28a6689593388
SJCE7-PC\SJCE7 <pjh6274@gmail.com>
2016-10-02 18:29:41 +0900

    stage

---
+++ Test/Assets/buildManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buildManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("dsfd");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
