f849882421372577253138a1d24b94040541d132
limSG <limsg0510@gmail.com>
2016-10-09 15:12:29 +0900

    터치 테스트

--- Test/Assets/buildManager.cs
+++ Test/Assets/buildManager.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class buildManager : MonoBehaviour {
 
+    public Ray ray;
+    public RaycastHit hitInfo;
+
 	// Use this for initialization
 	void Start () {
+
+        Debug.Log("test");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.gameObject.name); //gameObject name
+            }
+        }
 	}
 }
