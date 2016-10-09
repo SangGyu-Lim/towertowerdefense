2be15c4edcd248e00560d0339deee195df9fbe19
limSG <limsg0510@gmail.com>
2016-10-09 20:33:58 +0900

    tower panel

--- Test/Assets/buildManager.cs
+++ Test/Assets/buildManager.cs
@@ -6,10 +6,32 @@ public class buildManager : MonoBehaviour {
     Ray ray;
     RaycastHit hitInfo;
 
+    public GameObject goTowerPanel;
+
+    /* test
+    public int count;
+    public GameObject[] towerList;
+    public GameObject goTower;
+    public GameObject goParent;
+    int temp = 0;
+
+    const float RANGE_MIN = -10.0F;
+    const float RANGE_MAX = 10.0F;
+
+    int posX = 100;
+    int posZ = 100;
+    */
+    
 	// Use this for initialization
 	void Start () {
         Debug.Log("buildManager start");
-	}
+
+
+        /* test
+        towerList = new GameObject[count];
+        towerInit();
+        */
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,7 +43,39 @@ public class buildManager : MonoBehaviour {
             if (Physics.Raycast(ray, out hitInfo))
             {
                 Debug.Log(hitInfo.transform.gameObject.name);
+                if (hitInfo.transform.gameObject.name == "box")
+                    goTowerPanel.SetActive(true);
+                
+                
+                //Debug.Log();
             }
         }
 	}
+
+
+    /* test
+    void towerInit()
+    {
+        for (int i = 0; i < towerList.Length; ++i)
+        {
+
+            towerList[i] = GameObject.Instantiate(goTower) as GameObject;
+            towerList[i].transform.parent = goParent.transform;
+
+            towerList[i].name = temp.ToString();
+
+            towerList[i].transform.localScale = new Vector3(356, 356, 356);
+
+            float x = Random.Range(RANGE_MIN, RANGE_MAX);
+            float z = Random.Range(RANGE_MIN, RANGE_MAX);
+
+            towerList[i].transform.position = Vector3.zero;
+
+            towerList[i].transform.localPosition = new Vector3((posX + 50 * temp), (posZ + 50 * temp), 0f);
+            towerList[i].SetActive(true);
+
+            temp++;
+        }
+    }
+    */
 }
