34a44115cadabb2dcb035bde0af92bdd482ce097
limSG <limsg0510@gmail.com>
2016-10-09 17:54:13 +0900

    카메라 추가

--- Test/Assets/buildManager.cs
+++ Test/Assets/buildManager.cs
@@ -3,12 +3,25 @@ using System.Collections;
 
 public class buildManager : MonoBehaviour {
 
+    Ray ray;
+    RaycastHit hitInfo;
+
 	// Use this for initialization
 	void Start () {
+        Debug.Log("buildManager start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                Debug.Log(hitInfo.transform.gameObject.name);
+            }
+        }
 	}
 }
