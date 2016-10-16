f41872179496252200fee5c0835d02d83b5b5e9a
limSG <limsg0510@gmail.com>
2016-10-16 20:06:52 +0900

    파일 입출력

--- Test/Assets/buildManager.cs
+++ Test/Assets/buildManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class buildManager : MonoBehaviour {
 
@@ -42,6 +43,7 @@ public class buildManager : MonoBehaviour {
         Debug.Log("buildManager awake");
 
         initValue();
+        //fileWrite();
         fileLoad();
 
         //towerInfo = new sTower[5];
@@ -214,6 +216,36 @@ public class buildManager : MonoBehaviour {
 
     void fileLoad()
     {
+        StreamReader sr = new StreamReader("testFileInputOutput.txt");
+        string str;
+
+        str = sr.ReadLine();
+        int towerCount = int.Parse(str);
+        towerInfo = new sTower[towerCount];
+
+        for (int i = 0; i < towerCount; ++i )
+        {
+            str = sr.ReadLine();
+            if (str == null) break;
+            else
+            {
+                string[] dataTexts = str.Split('\t');
+                
+                towerInfo[i].name = dataTexts[0];
+                towerInfo[i].atk = int.Parse(dataTexts[1]);
+                towerInfo[i].range = float.Parse(dataTexts[2]);
+            }
+        }
+    }
+
+    void fileWrite()
+    {
+        StreamWriter sw = new StreamWriter("testFileInputOutput.txt");
+        sw.WriteLine("line write");
+        sw.Write("write");
+        sw.WriteLine("line write");
+        sw.Flush();
+        sw.Close();
     }
 
     
